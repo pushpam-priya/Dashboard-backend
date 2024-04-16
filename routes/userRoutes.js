cb36ec62975beed6777144e8f87ff591cf07cd2c
pushpam.priya <pushpam.priya@indicchain.com>
2024-04-16 18:44:36 +0530

    userRouter changes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -2,11 +2,11 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, currentUser } = require("../controllers/userController.js")
 
-router.post("/register",registerUser)
+router.post("/register",registerUser);
 
-router.post("/login",loginUser)
+router.post("/login",loginUser);
  
-router.get("/current",currentUser)
+router.get("/current",currentUser);
  
 
-module.exports = router 
+module.exports = router; 
